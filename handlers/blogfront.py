02927e7f8b76a079ca49d524d9f1f3575cab15f0
pm0355 <pm0355a@student.american.edu>
2017-01-26 15:14:14 -0500

    blog, newpost, refactor

--- handlers/blogfront.py
+++ handlers/blogfront.py
@@ -2,6 +2,9 @@ from google.appengine.ext import db
 from handlers.blog import BlogHandler
 
 class BlogFrontHandler(BlogHandler):
+
     def get(self):
-        posts = greetings = Post.all().order('-created')
-        self.render('front.html', posts = posts)
+        posts = db.GqlQuery(
+            "select * from Post order by created desc limit 10")
+
+        self.render('front.html', posts=posts)
