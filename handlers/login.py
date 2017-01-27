474d27479e80ecec1a01b69f6e410a5133571259
pm0355 <pm0355a@student.american.edu>
2017-01-27 09:12:57 -0500

    login/logout form correction

--- handlers/login.py
+++ handlers/login.py
@@ -2,6 +2,21 @@ from handlers.blog import BlogHandler
 from models.user import User
 
 
+
 class LoginHandler(BlogHandler):
+
     def get(self):
         self.render('login-form.html')
+
+    def post(self):
+        username = self.request.get('username')
+        password = self.request.get('password')
+
+        u = User.login(username, password)
+
+        if u:
+            self.login(u)
+            self.redirect('/blog')
+        else:
+            error = 'Invalid Username or Password'
+            self.render('login-form.html', error=error)
